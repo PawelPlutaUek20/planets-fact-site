b7194d57e49f44ae03ad72e5069fe83cb5f90758
pawelplutauek20 <pawelpluta58@gmail.com>
2021-07-21 16:54:04 +0200

    desktop and tablet navbar style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+const useStyles = makeStyles((theme) => ({
+  [theme.breakpoints.up('md')]:{
+    planets: {
+      width: "fit-content",
+    },
+    menu: {
+      justifyContent: "space-between"
+    }
+  },
+  [theme.breakpoints.only('sm')]:{
+    planets: {
+      justifyContent: "center"
+    },
+    menu: {
+      justifyContent: "center"
+    }
+  },
+  [theme.breakpoints.only('xs')]:{
+    planets: {
+      display: "none"
+    }
+  },
+  planet: {
+    fontSize: "0.7rem",
+    letterSpacing: "0.1em",
+    fontWeight: 700,
+    color: "rgba(255,255,255,0.75)",
+    textTransform: "uppercase",
+    minWidth: 80,
+    '&:hover': {
+      color: "rgba(255,255,255,1)"
+    }
+  },
+  navBar: {
+    backgroundColor: "transparent",
+    borderBottom: "solid 1px rgba(255, 255, 255, 0.2)",
+  },
+  0: {
+    background: "#419EBB",
+  },
+  1:{
+    background: "#EDA249"
+  },
+  2:{
+    background: "#6D2ED5"
+  },
+  3:{
+    background: "#D14C32"
+  },
+  4:{
+    background: "#D83A34"
+  },
+  5:{
+    background: "#CD5120"
+  },
+  6:{
+    background: "#1EC1A2"
+  },
+  7:{
+    background: "#2D68F0"
+  },
+}));
 
 const Navbar = ({ planets, setPlanet }) => {
+  const classes = useStyles();
+
+  const [value, setValue] = useState(2);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <nav>
-      <h2>THE PLANETS</h2>
-      <ul>
-        {planets.map((planet, planetIdx) => (
-          <li key={planetIdx}>
-            <a
-              href="#/"
-              onClick={() =>
-                setPlanet(planets.find((e) => e.name === planet.name))
-              }
-            >
-              {planet.name}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <AppBar className={classes.navBar} position="static">
+      <Toolbar>
+        <Grid item container alignItems="center" className={classes.menu}>
+          <Grid item>
+            <h2>THE PLANETS</h2>
+          </Grid>
+          <Grid item container className={classes.planets}>
+            <Tabs TabIndicatorProps={{className:classes[value]}} value={value} onChange={handleChange}>
+              {planets.map((planet, planetIdx) => (
+                <Tab
+                  key={planetIdx}
+                  className={classes.planet}
+                  onClick={() =>
+                    setPlanet(planets.find((e) => e.name === planet.name))
+                  }
+                  label={planet.name}
+                />
+              ))}
+            </Tabs>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,49 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import backgroundStars from "./assets/background-stars.svg";
+import { createTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 700,
+      md: 1025,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  palette: {
+    background: {
+      default: "#070724",
+    },
+    text: {
+      primary: "white",
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          backgroundImage: `url(${backgroundStars})`,
+        },
+      },
+    },
+    MuiTab: {
+      root: {
+        padding: "28px 12px",
+      },
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
